6e90343f88607205b1666787c326e53cb09e95ee
miniufo <miniufo.163.com>
2023-04-21 19:58:11 +0200

    add docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,6 +88,9 @@ html_theme_options = {
     'navigation_depth': 4,
     'prev_next_buttons_location': 'bottom',  # top and bottom
 }
+html_css_files = [
+    'my_theme.css',
+]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
